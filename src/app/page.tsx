58a79ea2af41c4d849bd54340813468bfb60c33f
Nephi <nephi@nephrited.com>
2024-12-02 20:07:14 +0000

    Minor CSS changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,9 @@ import {SessionProvider} from "next-auth/react";
 export default function Home() {
     return (
         <SessionProvider>
-            <div
-                className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-                <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                    <h1>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <main className="flex flex-col gap-8 row-start-2 items-center pl-2 pr-2 max-w-lg">
+                    <h1 className="text-2xl font-bold mt-2">
                         Shift Secret Santa
                     </h1>
                     <UserControls/>
